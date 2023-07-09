da888daa9bf05880a9d112eba324f02e406552e5
Mary Pankova <flam.mary@gmail.com>
2023-07-09 11:49:27 -0700

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Mariya Pankova on 09.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
