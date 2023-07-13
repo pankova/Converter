238d722ea60ecf477817520dc0bc06fa7e53fec2
Mary Pankova <flam.mary@gmail.com>
2023-07-12 19:40:36 -0700

    SegmentView

--- Converter/View/SegmentView.swift
+++ Converter/View/SegmentView.swift
@@ -7,14 +7,48 @@
 
 import SwiftUI
 
+enum Segment: String, CaseIterable {
+    case weight, length, temperature, currency, distance, area
+}
+
 struct SegmentView: View {
+    var segments: [Segment]
+    @State var selected: Segment
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            ScrollViewReader { proxy in
+                HStack {
+                    ForEach(segments, id: \.self) { segment in
+                        ZStack {
+                            Text(segment.rawValue.capitalized)
+                                .id(segment)
+                                .padding(6)
+                                .background(selected == segment ? .green : .white)
+                                .cornerRadius(8)
+                                .clipShape(ContainerRelativeShape())
+                                .gesture(
+                                    TapGesture()
+                                        .onEnded{ gesture in
+                                            selected = segment
+                                            withAnimation {
+                                                proxy.scrollTo(segment, anchor: .center)
+                                            }
+                                        }
+                                )
+                        }
+                    }
+                }
+                .padding(Constants.padding)
+            }
+            .background(.gray)
+        }
+        .frame(height: 60)
     }
 }
 
 struct SegmentView_Previews: PreviewProvider {
     static var previews: some View {
-        SegmentView()
+        SegmentView(segments: Segment.allCases, selected: .weight)
     }
 }
